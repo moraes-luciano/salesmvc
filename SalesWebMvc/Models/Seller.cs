707b947aca0995e7fe0219886cad3ec8661f45f4
Luciano Moraes <pmoraes.luciano@gmail.com>
2022-04-08 20:59:03 -0300

    Location and data formats

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,11 +11,25 @@ namespace SalesWebMvc.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        
         public string Name { get; set; }
+        
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        
+        [Display(Name = "Base Salary")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public decimal BaseSalary { get; set; }
+        
         public Department Department { get; set; }
+        
         public int DepartmentId { get; set; }
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
